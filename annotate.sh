a8f816bc94a6a1c0bb2d6ed3553cb4b402847d08
Meghan Ford <78237587+meghanclownfish@users.noreply.github.com>
2025-01-30 20:21:17 -0500

    Create annotate.sh

---
+++ annotate.sh
@@ -0,0 +1,12 @@
+# using rna from SRA-SRX357400
+
+# trim and filter raw reads (version = 0.23.4)
+fastp -i combined_nucella_rna.fastq -o fastp_combined_nucella_rna.fastq -t 40
+
+
+##map reads to genome
+#make genome dir for STAR (version = )
+STAR --runThreadN 30 --runMode genomeGenerate --genomeDir /home/meghan/nucella_genome/annotate/index --genomeFastaFiles /home/meghan/nucella_genome/decontaminate/len1kb_20x_metazoa_polish3.fasta
+
+#map rna 
+
