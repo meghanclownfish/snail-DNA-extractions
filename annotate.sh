8c434bb5fe4d1d0be9d6ce0f9c3ac76a2d1617fd
Meghan Ford <78237587+meghanclownfish@users.noreply.github.com>
2025-01-31 11:53:54 -0500

    Update annotate.sh

--- annotate.sh
+++ annotate.sh
@@ -1,3 +1,4 @@
+conda 24.9.2
 # using rna from SRA-SRX357400
 
 # trim and filter raw reads (version = 0.23.4)
@@ -9,14 +10,13 @@ fastp -i combined_nucella_rna.fastq -o fastp_combined_nucella_rna.fastq -t 40
 STAR --runThreadN 30 --runMode genomeGenerate --genomeDir /home/meghan/nucella_genome/annotate/index --genomeFastaFiles /home/meghan/nucella_genome/decontaminate/len1kb_20x_metazoa_polish3.fasta
 
 #map rna 
-
 STAR --genomeDir /home/meghan/nucella_genome/annotate/index \
       --readFilesIn /home/meghan/nucella_genome/annotate/rna/fastp_combined_nucella_rna.fastq \
       --outSAMtype BAM SortedByCoordinate \
       --outFileNamePrefix mapped_fastp_rna
 
 #RepeatModeler (version = 2.0.6)
-BuildDatabase -name jc-genome JC-genome_v3-230128.fasta
+makeblastdb -name nucella_genome /home/meghan/nucella_genome/decontaminate/len1kb_20x_metazoa_polish3.fasta
 RepeatModeler -threads 32 -database jc-genome -LTRStruct
 
 
