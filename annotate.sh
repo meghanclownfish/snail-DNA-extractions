c4f10101aa4c2ed99f44e673b2243243a8ef763c
Meghan Ford <78237587+meghanclownfish@users.noreply.github.com>
2025-02-04 10:37:51 -0500

    Update annotate.sh

--- annotate.sh
+++ annotate.sh
@@ -4,19 +4,12 @@
 # trim and filter raw reads (fastp v0.23.4)
 fastp -i combined_nucella_rna.fastq -o fastp_combined_nucella_rna.fastq -t 40
 
-
-##map reads to genome
-#make genome dir for STAR (version = )
-STAR --runThreadN 30 --runMode genomeGenerate --genomeDir /home/meghan/nucella_genome/annotate/index --genomeFastaFiles /home/meghan/nucella_genome/decontaminate/len1kb_20x_metazoa_polish3.fasta
-
-#map rna 
-STAR --genomeDir /home/meghan/nucella_genome/annotate/index \
-      --readFilesIn /home/meghan/nucella_genome/annotate/rna/fastp_combined_nucella_rna.fastq \
-      --outSAMtype BAM SortedByCoordinate \
-      --outFileNamePrefix mapped_fastp_rna
-
 #RepeatModeler (version = 2.0.6)
+singularity pull dfam-tetools-latest.sif docker://dfam/tetools:latest #(DONT RUN AGAIN)
+
 conda activate repeatmodeler_env
+singularity run dfam-tetools-latest.sif
+
 BuildDatabase -name nucella_genome len1kb_20x_metazoa_polish3.fasta 
 nohup bash -c "RepeatModeler -LTRStruct -database nucella_genome -threads 38" >  Rmodler.log 2>&1 &
 
