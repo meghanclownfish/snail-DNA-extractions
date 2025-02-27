f7db0e00b75acd7191236f1ef8933c3cb9629c95
Meghan Ford <78237587+meghanclownfish@users.noreply.github.com>
2025-02-27 16:14:39 -0500

    Update and rename annotate.sh to annotate-boros.sh

--- annotate-boros.sh
+++ annotate-boros.sh
@@ -8,10 +8,10 @@ fastp -i combined_nucella_rna.fastq -o fastp_combined_nucella_rna.fastq -t 40
 singularity pull dfam-tetools-latest.sif docker://dfam/tetools:latest #(DONT RUN AGAIN)
 
 conda activate repeatmodeler_env
-singularity run dfam-tetools-latest.sif
+singularity run ../dfam-tetools-latest.sif
 
-BuildDatabase -name nucella_genome len1kb_20x_metazoa_polish3.fasta 
-nohup bash -c "RepeatModeler -LTRStruct -database nucella_genome -threads 38" >  Rmodler.log 2>&1 &
+BuildDatabase -name nucella_genome_no_scaffold hifi_2kb_decontaminated.fa 
+nohup bash -c "RepeatModeler -LTRStruct -database nucella_genome_no_scaffold -threads 32" >  Rmodler.log 2>&1 &
 
 RepeatMasker -pa 10 -lib families.fa -xsmall -gff len1kb_20x_metazoa_polish3.fasta
 
