31198dcc5faae91950ae680881045e91fe24bf1f
Meghan Ford <78237587+meghanclownfish@users.noreply.github.com>
2025-03-04 12:46:58 -0500

    Update annotate-boros.sh

--- annotate-boros.sh
+++ annotate-boros.sh
@@ -1,29 +1,19 @@
 #conda 24.9.2
 
-
-# trim and filter raw reads (fastp v0.23.4)
-fastp -i combined_nucella_rna.fastq -o fastp_combined_nucella_rna.fastq -t 40
-
 #RepeatModeler (version = 2.0.6)
 singularity pull dfam-tetools-latest.sif docker://dfam/tetools:latest #(DONT RUN AGAIN)
 
+#activate conda env with singularity
 conda activate repeatmodeler_env
 singularity run ../dfam-tetools-latest.sif
 
 BuildDatabase -name nucella_genome_no_scaffold hifi_2kb_decontaminated.fa 
-nohup bash -c "RepeatModeler -LTRStruct -database nucella_genome_no_scaffold -threads 32" >  Rmodler.log 2>&1 &
-
-#or 
 
 #start an instance 
-conda activate repeatmodeler_env
-
 singularity instance start ../dfam-tetools-latest.sif run_rm
 
-
+#run modeler and masker
 nohup singularity exec instance://run_rm RepeatModeler -LTRStruct -database nucella_genome_no_scaffold -threads 35 &
-
-
 nohup singularity exec instance://run_rm RepeatMasker -pa 35 -lib ../nucella_genome_no_scaffold-families.fa -xsmall -gff ../hifi_2kb_decontaminated.fa &
 
 
@@ -34,7 +24,15 @@ nohup singularity exec instance://run_rm RepeatMasker -pa 35 -lib ../nucella_gen
 
 ##firefly + new hifi
 
+#activate conda env with singularity
+conda activate repeatmodeler_env
+singularity run ../dfam-tetools-latest.sif
+
 BuildDatabase -name nucella_genome_new new_hifiasm_2kb.a_ctg.fa
 
+#start an instance 
+singularity instance start ../dfam-tetools-latest.sif run_rm
+
+#run modeler and masker
 nohup singularity exec instance://run_rm RepeatModeler -LTRStruct -database nucella_genome_new -threads 35 &
 
