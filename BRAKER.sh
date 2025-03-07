f15d71b1f2612839bf1bf80124113d49aba47c8e
Meghan Ford <78237587+meghanclownfish@users.noreply.github.com>
2025-03-07 00:10:39 -0500

    Update BRAKER.sh

--- BRAKER.sh
+++ BRAKER.sh
@@ -20,13 +20,15 @@ nohup singularity exec -B /home/meghan/nucella_genome/annotate/no_scaffold/v1_br
 --bam=/home/meghan/nucella_genome/annotate/no_scaffold/all_mapped_rna.bam \
 --AUGUSTUS_CONFIG_PATH=/home/meghan/config &
 
-#initial busco: 72.7%, running again with hints 
+#initial busco: 72.7%, running again with eukaryota instead of metazoa 
 
-nohup singularity exec -B /home/meghan/nucella_genome/annotate/no_scaffold/braker /home/meghan/braker3.sif braker.pl \
+nohup singularity exec -B /home/meghan/nucella_genome/annotate/no_scaffold/brakerv2 /home/meghan/braker3.sif braker.pl \
 --genome=/home/meghan/nucella_genome/annotate/no_scaffold/hifi_2kb_decontaminated.fa.masked \
---species=v1_nucella --threads=35 --useexisting --workingdir=braker_r2_v1 \
---hints=/home/meghan/nucella_genome/annotate/no_scaffold/braker/hintsfile.gff \
+--species=v2_nucella  --softmasking --threads=35 \
+--prot_seq=/home/meghan/nucella_genome/database/eukaryota_and_molluscan_protien.fasta \
+--bam=/home/meghan/nucella_genome/annotate/no_scaffold/masked_all_mapped_rna.bam \
 --AUGUSTUS_CONFIG_PATH=/home/meghan/config &
+
 
 #entap on firefly 
 
